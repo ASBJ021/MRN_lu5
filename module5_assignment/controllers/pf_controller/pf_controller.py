c47a72a3e93db35f81e98242399fb24488ebc851
Akash Bhansali <35609643+ASBJ021@users.noreply.github.com>
2024-11-23 11:17:34 +0100

    minor fixes

--- module5_assignment/controllers/pf_controller/pf_controller.py
+++ module5_assignment/controllers/pf_controller/pf_controller.py
@@ -458,19 +458,18 @@ def add_random_particles(particles, weights, map_limits):
     Returns:
         list: New set of particles.
     """
-    # Ensure there are particles and weights
     num_particles = len(particles)
-    num_replace = num_particles // 2  # Number of particles to replace
+    num_replace = num_particles // 2 
 
-    # Sort particles by their weights (ascending order)
-    sorted_indices = np.argsort(weights)
-    indices_to_replace = sorted_indices[:num_replace]
+    particle_with_weights = list(zip(range(num_particles),weights))
+    
+    sorted_particles = sorted(particle_with_weights,key=lambda x: x[1])
 
     # Replace particles with lowest weights
     new_particles = particles.copy()
-    for idx in indices_to_replace:
+    for i in range(num_replace):
+        idx = sorted_particles[i][0]
         new_particles[idx] = sample_random_particle(map_limits)
-
     return new_particles
 
 
